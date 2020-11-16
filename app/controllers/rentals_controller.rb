4fa3041874c36e7333c339f73d7b6112200929fa
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-16 16:51:16 -0500

    rental controller almost done

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,30 @@
 class RentalsController < ApplicationController
+  def new
+    @rental = Rental.new
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @tootbrush = Tootbrush.find(params[:toothbrush_id])
+    @user = User.find(params[:user_id])
+    @rental.user = @user
+    @rental.toothbrush = @toothbrush
+    @rental.save
+
+    redirect_to toothbrush_path(@toothbrush)
+  end
+
+  def index
+    @rentals = Rental.all
+  end
+
+  def show
+    @rental = Rental.find(params[:id])
+  end
+
+  private
+
+  def rental_params
+    params.require(:tootbrush).permit(:user_id, :toothbrush_id)
+  end
 end
