ce83edc0f9ba5b166fd6bbe37441e3a2e08badec
Javier Granda <javogroj@gmail.com>
2020-11-16 15:23:02 -0700

    added new and create to controller and view form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  def new
+    @toothbrush = toothbrush.find(params[:toothbrush_id])
+    @review = Review.new
+  end
+
+  def create
+    @toothbrush = toothbrush.find(params[:toothbrush_id])
+    @review = Review.new(review_params)
+    @review.content = @content
+    @review.rating = review_params["rating"].to_i
+    if @review.save
+      redirect_to #should ask where
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
