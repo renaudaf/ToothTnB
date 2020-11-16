995668036867154607c29cee62cae28a2f9a3af4
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-16 16:23:43 -0500

    validations on rental, review and toothbrush

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,6 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :toothbrush
+  validates :status, inclusion: { in: %w(Accepted Denied Pending),
+    message: "%{value} is not a valid category" }
 end
--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -1,5 +1,9 @@
 class Toothbrush < ApplicationRecord
-  has_many :reviews
-  has_many :rentals
+  has_many :reviews, dependent: :destroy
+  has_many :rentals, dependent: :destroy
   belongs_to :user, through: :rentals
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :status, inclusion: { in: ["Available", "Not Available"],
+    message: "%{value} is not a valid category" }
 end
