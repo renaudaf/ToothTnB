27f5786887446dea3e6ed40146ca5e54134ec341
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-17 15:06:24 -0500

    added the login obligations for all portions of the site except the index page

--- app/controllers/toothbrushes_controller.rb
+++ app/controllers/toothbrushes_controller.rb
@@ -1,4 +1,6 @@
 class ToothbrushesController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @toothbrushes = Toothbrush.all
